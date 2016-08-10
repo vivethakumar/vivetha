4600173b474c7d4aea271bfad9ef3dfea0148d72
vivethakumar <vivethavive51@gmail.com>
2016-08-10 15:11:21 +0530

    Update hunter_10.java

--- hunter_10.java
+++ hunter_10.java
@@ -5,13 +5,21 @@ class hunter_10
 {
 public static void main(String args[])
 {
+ String s1="":
 int a1[]=new int[]{1,2,3};
 int a2[]=new int[]{2,3,1,2,3,5};
 boolean flag=false;
+for(int i=0;i<a1.length;i++)
+{
+ s1+=String.valueOf(a1[i]);
+}
+for(int j=0;j<a2.length();j++)
+{
+ s2+=String.valueOf(a2[j]);
+}
+Sytem.out.println(s1+" "+s2);
 if(a1.length<a2.length)
 {
-String s1=String.valueOf(a1);
-String s2=String.valueOf(a2);
 for(int i=0;i<s2.length();i++)
 {
 if(s2.regionMatches(i,s1,0,s1.length())
