fd86e8cf45c0c5fde7549de325f84d1a60c80597
vivethakumar <vivethavive51@gmail.com>
2016-08-12 18:58:25 +0530

    Update hunter_63.java

--- hunter_63.java
+++ hunter_63.java
@@ -12,15 +12,23 @@ a[i]=s.nextInt();
 }
 for(int i=0;i<n;i++)
 {
+  boolean flag=false;
+  if(i==n-2)
+  {
+    a[i]=a[i+1];flag=true;
+  }
 if(i==n-1)
 {
 a[i]=0;break;
 }
+if(!flag)
+{
 for(int j=i+1;j<n;j++)
 {
-if(a[i]<a[j])
+if(a[j-1]<a[j])
 {
 a[i]=a[j];break;
+}
 }
 }
 }
