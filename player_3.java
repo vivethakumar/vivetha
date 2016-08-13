3af85e8e7b0568864e9709939154a63de7ff49eb
vivethakumar <vivethavive51@gmail.com>
2016-08-13 14:13:27 +0530

    Update player_3.java

--- player_3.java
+++ player_3.java
@@ -3,12 +3,13 @@ class player_3
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
-int n=s.nextInt();
+Scanner s1=new Scanner(System.in);
+int n=s1.nextInt();
 if(n>9||n<(-9))
 {
 String s=String.valueOf(n);
-StringBuilder ss=new StringBuilder(s).reverse();
+StringBuilder ss=new StringBuilder(s);
+ss.reverse();
 n=Integer.parseInt(ss.toString());
 System.out.println(n);
 }
