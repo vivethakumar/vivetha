94c47732f56471cbd60a4526348c457b73ac9b2d
vivethakumar <vivethavive51@gmail.com>
2016-08-13 13:51:49 +0530

    Create player_3.java

---
+++ player_3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class player_3
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n>9||n<9)
+{
+String s=String.valueOf(n);
+StringBuilder ss=new StringBuilder(s).reverse();
+n=Integer.parseInt(ss.toString());
+System.out.println(n);
+}
+else
+System.out.println(n);
+}
+}
