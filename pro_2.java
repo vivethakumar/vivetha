16c93e3c1d57661754632564facc258b45066760
vivethakumar <vivethavive51@gmail.com>
2016-08-13 14:30:38 +0530

    Create pro_2.java

---
+++ pro_2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class pro_2
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int k=s.nextInt();
+String s1=String.valueOf(n),ss="";
+String[] a=s1.split("");
+if(n>0)
+{
+for(int i=0;i<a.length;i++)
+{
+for(int j=i+1;j<a.length;j++)
+{
+if(Integer.parseInt(a[i])<Integer.parseInt(a[j]))
+{
+int temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+}
+for(int i=0;i<a.length;i++)
+{
+ss+=a[i];
+}StringBuffer str=new StringBuffer(ss);
+for(int i=a.length-1;i>k;i--)
+{ss="";
+str=str.deleteCharAt(i);
+}
+ss=str.toString();
+n=Integer.ParseInt(ss);
+System.out.println(n);
+}
+}
