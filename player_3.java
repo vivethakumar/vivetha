1d98a1bd51000b9d86c4fb34e2dff476a8d19a73
vivethakumar <vivethavive51@gmail.com>
2016-08-13 13:52:35 +0530

    Update

--- player_3.java
+++ player_3.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
-if(n>9||n<9)
+if(n>9||n<(-9))
 {
 String s=String.valueOf(n);
 StringBuilder ss=new StringBuilder(s).reverse();
