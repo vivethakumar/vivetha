db314029ca4b88eb824e07e9431a250c7ec9e2cc
vivethakumar <vivethavive51@gmail.com>
2016-08-22 19:02:36 +0530

    Update hunter_51.java

--- hunter_51.java
+++ hunter_51.java
@@ -6,8 +6,8 @@ class hunter_51
    Scanner s=new Scanner(System.in);
    String num1=s.next();
    String num2=s.next();
-   if(Integer.parseInt(num1)==0||Integer.parseInt(num2)==0)
-   System.out.println(0);
+   if(Integer.parseInt(num1)<0||Integer.parseInt(num2)<0)
+   System.out.println("number is negative");
    else
    System.out.println(Integer.parseInt(num1)*Integer.parseInt(num2));
    }
