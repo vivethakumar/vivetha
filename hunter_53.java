92e931e874c90f0cc56eabb6010d46439ac00d58
vivethakumar <vivethavive51@gmail.com>
2016-08-22 19:17:00 +0530

    Update hunter_53.java

--- hunter_53.java
+++ hunter_53.java
@@ -5,11 +5,18 @@ class hunter_53
     {
     Scanner s=new Scanner(System.in);
     String str=s.nextLine();
+    int len=s.nextInt();
     if(str.length()>=1)
     {
+        String s1="p";
     for(int i=0;i<str.length();i++)
     {
-       System.out.print("p"+str.charAt(i));
+        s1+=str.charAt(i);
+       if(s1.length()==len)
+       {
+       System.out.print(s1);
+       s1="p";
+       }
        System.out.print((i==str.length()-1)?"":" ");
     }
     }
