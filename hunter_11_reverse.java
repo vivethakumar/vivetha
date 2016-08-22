677c1e7be8f80542b78e8b0c71249bc4f255c30e
vivethakumar <vivethavive51@gmail.com>
2016-08-22 18:05:54 +0530

    Update and rename hunter_11.java to hunter_11_reverse.java

---
+++ hunter_11_reverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hunter_11_reverse
+{
+   public static void main()
+   {
+   Scanner s=new Scanner(System.in);
+   String str=s.nextLine();
+   StringBuffer s1=new StringBuffer(str);
+   s1.reverse();
+   System.out.println(s1.toString());
+   }
+}
