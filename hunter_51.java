53be2e6a6c85195997843e42a45986f048efc0b1
vivethakumar <vivethavive51@gmail.com>
2016-08-22 19:01:06 +0530

    Create hunter_51.java

---
+++ hunter_51.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hunter_51
+{
+   public static void main(String[] args)
+   {
+   Scanner s=new Scanner(System.in);
+   String num1=s.next();
+   String num2=s.next();
+   if(Integer.parseInt(num1)==0||Integer.parseInt(num2)==0)
+   System.out.println(0);
+   else
+   System.out.println(Integer.parseInt(num1)*Integer.parseInt(num2));
+   }
+}
