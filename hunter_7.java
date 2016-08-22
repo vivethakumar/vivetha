c50dd522c2791268c7ac960c0a56379d7b44fb29
vivethakumar <vivethavive51@gmail.com>
2016-08-22 17:43:50 +0530

    Create hunter_7.java

---
+++ hunter_7.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class hunter_7
+{
+   public static void main(String args[])
+   {
+   Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   int a[]new int[n];
+   Map<Integer,Integer> set=new LinkedHashMap<Integer,Integer>();
+   for(int i=0;i<n;i++)
+   {
+   a[i]=s.nextInt();
+   if(!set.contains(a[i]))
+      set.put(a[i],1);
+   else
+     set.put(a[i],set.get(a[i])+1);
+   }
+   Set ss=set.entrySet();
+   Iterator it=set.iterator();
+   while(it.hasNext())
+   {
+     Map.Entry e=(Map.Entry)it.next();
+     if(e.getvalue==1)
+     System.out.println(e.getkey());
+   }
+   }
+}
