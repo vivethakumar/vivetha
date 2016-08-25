a9803ca8604f68d95929dbff37e94a19cb75c858
vivethakumar <vivethavive51@gmail.com>
2016-08-25 11:29:26 +0530

    Update hunter_55.java

--- hunter_55.java
+++ hunter_55.java
@@ -8,11 +8,15 @@ class hunter_55
         int a[]=new int[n];
         int k=s.nextInt();
         String rotate="";
+        if(n>0)
+        {
         for(int i=0;i<n;i++)
         {
         a[i]=s.nextInt();
         rotate+=String.valueOf(a[i]);
         }
+        if(k<n)
+        {
         String sub=rotate.substring(k,rotate.length());
         StringBuffer s1=new StringBuffer(rotate);
         s1=s1.delete(k,rotate.length());
@@ -23,6 +27,12 @@ class hunter_55
         int m=Integer.parseInt(String.valueOf(rotate.charAt(i)));
           System.out.print(m+""+(i==rotate.length()-1?"":" "));
         }
+        }
+        else
+           System.out.println("cant able to rotate");
+        }
+        else
+          System.out.println("invalid");
         
         
     }
