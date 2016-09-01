634d7b1ddf8188fd2d8aaec179bcf93f4fb1ca18
vivethakumar <vivethavive51@gmail.com>
2016-09-01 15:26:58 +0530

    Create player_148.java

---
+++ player_148.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class player_148
+{
+      public static void main(String[] args)
+      {
+         Scanner s=new Scanner(Syste.in);
+         int n=s.nextInt();
+         String str="";
+         for(int i=0;i<n;i++)
+         {
+             if(i%2!=0)
+                 str+=String.valueOf(i);
+         }
+         n=Integer.parseInt(String.valueOf(str.charAt(0)))+Integer.parseInt(String.valueOf(str.charAt(0)));
+         for(int i=2;i<str.length();i++)
+         {
+            int b=Integer.parseInt(String.valueOf(str.charAt(i)));
+            if(i%2==0)
+               n-=b;
+            else
+               n+=b;
+         }
+         System.out.println(n);
+      }
+}
