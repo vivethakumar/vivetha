940852b5b1a11abf2ee2fb2b8e454566101fc293
vivethakumar <vivethavive51@gmail.com>
2016-09-07 12:01:52 +0530

    Update hunter_88.java

--- hunter_88.java
+++ hunter_88.java
@@ -4,16 +4,22 @@ class hunter_88
     public static void main(String[] args)
     {
        Scanner s=new Scanner(System.in);
-       String a=s.nextInt();
+       String a=s.nextLine();
        ArrayList<Character> list=new ArrayList<Character>();
        String out="";
+       if(a.length()>1)
+       {
        for(int i=a.length()-1;i>=0;i--)
        {
-         if(!list.containsOf(a.charAt(i))
+         if(!list.contains(a.charAt(i)))
          {
          out+=String.valueOf(a.charAt(i));
+         list.add(a.charAt(i));
          }
        }
        System.out.println(out);
+       }
+       else
+         System.out.println(a);
     }
 }
