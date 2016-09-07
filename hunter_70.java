1c07cb6f99ea95de164ed8f676ea603e6a86aba5
vivethakumar <vivethavive51@gmail.com>
2016-09-07 12:14:18 +0530

    Update hunter_70.java

--- hunter_70.java
+++ hunter_70.java
@@ -1,4 +1,22 @@
 import java.util.*;
 class hunter_70
 {
+     public static void main(String[] args)
+     {
+       Scanner s=new Scanner(System.in);
+       int n=s.nextInt();
+       int[] a=new int[n];
+       int target=s.nextInt();
+       int m=s.nextInt();
+       Random r=new Random();
+       ArrayList<Integer> a1=new ArrayList<Integer>();
+       int fact=1;
+       while(m!=0)
+       {
+         fact*=m;
+         m--;
+       }
+       for(int i=0;i<fact;i++)
+         a1.add()
+     }
 }
